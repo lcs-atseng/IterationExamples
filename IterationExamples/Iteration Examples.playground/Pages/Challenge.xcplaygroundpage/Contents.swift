7e7528d45d111895a218a3a467a55d4bf3c070a0
Alice Tseng <atseng@lcs-mba3235.lcs.on.ca>
2017-10-10 15:25:13 -0400

    Processing Iteration Examples

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -16,7 +16,7 @@ let canvas = Canvas(width: 300, height: 300)
 
 // Below this line, try combining a loop and four statements that draw lines to generate the goal
 
-for i in stride(from: 0, through: 300, by: 20) {
+for i in stride(from: 0, through: 300, by:20) {
     canvas.drawLine(fromX: i, fromY: 0, toX: 300, toY: i);canvas.drawLine(fromX: 0, fromY: i, toX: 300-i, toY: 0);canvas.drawLine(fromX: i, fromY: 300, toX: 300, toY: 300-i);canvas.drawLine(fromX: 0, fromY: i, toX: i, toY: 300)
 }
 
